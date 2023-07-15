281fce7301d73e5b85216e27686816a12b3a9ecc
Ábel Fóthi <33127860+FAbika90@users.noreply.github.com>
2023-07-15 14:34:42 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+classifiers = [
+“Development Status :: 5 — Production/Stable”,
+“Intended Audience :: Science/Research”,
+“Operating System :: Unix”,
+“License :: OSI Approved :: MIT License”,
+“Programming Language :: Python :: 3”,
+]
+setup(name=””,version=”0.0.1",
+description=”RRBS tool for deleting artificial cytosins”,
+long_description=open(“README.txt”).read() + “\n\n” + open(“CHANGELOG.txt”).read(),
+url=””,
+author=””,
+author_email=””,
+license=”MIT”,
+classifiers=classifiers,
+keywords=””,
+packages=find_packages(),
+)
