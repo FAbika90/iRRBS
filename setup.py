6129153d47465b9868049be047fc748b662cdd8b
Ábel Fóthi <33127860+FAbika90@users.noreply.github.com>
2023-07-15 14:36:57 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ classifiers = [
 “License :: OSI Approved :: MIT License”,
 “Programming Language :: Python :: 3”,
 ]
-setup(name=””,version=”0.0.1",
+setup(
+name=”iRRBS”,
+version=”0.0.1",
 description=”RRBS tool for deleting artificial cytosins”,
 long_description=open(“README.txt”).read() + “\n\n” + open(“CHANGELOG.txt”).read(),
 url=””,
-author=””,
-author_email=””,
+author=”Abel Fothi”,
+author_email=”fothi.abel@gmail.com”,
 license=”MIT”,
 classifiers=classifiers,
 keywords=””,
