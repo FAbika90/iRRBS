a73fffa5b112647828edc6ea91e310e44b758980
Ábel Fóthi <33127860+fothia@users.noreply.github.com>
2023-07-27 17:19:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
 classifiers = [
-'Development Status :: 5 — Production/Stable',
+'Development Status :: 5 - Production/Stable',
 'Intended Audience :: Science/Research',
 'Operating System :: Unix',
 'License :: OSI Approved :: MIT License',
@@ -8,11 +11,11 @@ classifiers = [
 ]
 setup(
 name = 'iRRBS',
-version = '0.0.2',
+version = '0.0.3',
 description = 'RRBS tool for deleting artificial cytosins',
-long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+long_description = (this_directory / "README.md").read_text(),
 url = 'https://github.com/fothia/iRRBS',
-download_url = 'https://github.com/fothia/iRRBS/archive/refs/tags/0.0.2.tar.gz'
+download_url = 'https://github.com/fothia/iRRBS/archive/refs/tags/0.0.3.tar.gz'
 author = 'Abel Fothi',
 author_email = 'fothi.abel@gmail.com',
 license = 'MIT',
