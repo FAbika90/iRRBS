c92bd9103f487dc0c4d5ff118eb29722605a65af
Ábel Fóthi <33127860+fothia@users.noreply.github.com>
2023-07-27 16:53:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup, find_packages
 classifiers = [
-“Development Status :: 5 — Production/Stable”,
-“Intended Audience :: Science/Research”,
-“Operating System :: Unix”,
-“License :: OSI Approved :: MIT License”,
-“Programming Language :: Python :: 3”,
+'Development Status :: 5 — Production/Stable',
+'Intended Audience :: Science/Research',
+'Operating System :: Unix',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3',
 ]
 setup(
-name=”iRRBS”,
-version=”0.0.1",
-description=”RRBS tool for deleting artificial cytosins”,
-long_description=open(“README.md”).read() + “\n\n” + open(“CHANGELOG.txt”).read(),
-url=”https://github.com/fothia/iRRBS”,
-author=”Abel Fothi”,
-author_email=”fothi.abel@gmail.com”,
-license=”MIT”,
-classifiers=classifiers,
-keywords=””,
-packages=find_packages(),
+name = 'iRRBS',
+version = '0.0.2',
+description = 'RRBS tool for deleting artificial cytosins',
+long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+url = 'https://github.com/fothia/iRRBS',
+download_url = 'https://github.com/fothia/iRRBS/archive/refs/tags/0.0.2.tar.gz'
+author = 'Abel Fothi',
+author_email = 'fothi.abel@gmail.com',
+license = 'MIT',
+classifiers = classifiers,
+keywords = '',
+install_requires = [
+          'pysam',
+          'pybedtools',
+      ],
 )
